d1c989bc3786cc37e3369d01bc461ead84b4497d
ram <ram.awadhesh@gspann.com>
2014-06-06 18:47:23 +0530

    Minor changes

--- GRetail/HomeViewController.m
+++ GRetail/HomeViewController.m
@@ -29,7 +29,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    self.title = @"GSPANN Retail App";
+    self.title = @"Beacons";
     
     [self initializeTransmitters];
     
